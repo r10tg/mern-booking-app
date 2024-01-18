6fe39f089dffb0a8fe81fe1bcefb1f5d53e3315b
keyur gupta <136456609+r10tg@users.noreply.github.com>
2024-01-18 12:49:44 +0530

    Update auth.js
    tried adding samesite none to login route to check if it working

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -36,6 +36,7 @@ router.post(
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "None",
       });
       return res.status(200).send({userId:user._id});
     } catch (error) {
